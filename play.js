bb9c1c67351462b3a0bb0c1d05f0203885ab7a7d
gmarkoski <gary_markoski@hotmail.com>
2021-12-02 02:03:22 -0700

    updating with changes

--- play.js
+++ play.js
@@ -1,41 +1,11 @@
 const {connect} = require('./client');
 // const {setupInput} = require('./input');
+const {setupInput} = require('./input');
+
 
 console.log("Connecting ...");
-const conn = connect();
-
-const setupInput = function() {
-  const connection = conn;
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  
-  stdin.on("data", handleUserInput);
-  
-  return stdin;
-};
-
-const handleUserInput = function(data) {
-  if (data === 'i') {
-    connection.write("Move up: ");
-  }
-
-  if (data === 'j') {
-    connection.write("Move left: ");
-  }
-
-  if (data === 'm') {
-    connection.write("Move down: ");
-  }
-
-  if (data === 'l') {
-    connection.write("Move right: ");
-  }
-};
-  
-};
+connect();
 
 
 
-// setupInput(conn);
+setupInput();
