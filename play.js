df787573479e63e5ec7e6361b4dfb6db9f20ae24
gmarkoski <gary_markoski@hotmail.com>
2021-12-02 00:03:56 -0700

    updating

--- play.js
+++ play.js
@@ -1,22 +1,7 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: 50541,
-  });
-  
-  conn.on('connect', () => {
-    // code that does something when the connection is first established
-    console.log("Let us play the game!");
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  
-  return conn;
-};
+const {connect} = require('./client');
+// const {setupInput} = require('./input');
 
 console.log("Connecting ...");
-connect();
+const conn = connect();
+
+// setupInput(conn);
